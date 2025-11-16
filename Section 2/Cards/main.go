bb88c2f41fb2c5a384d2f7311e7660e683578976
thisismemukul <w41k3r.12@gmail.com>
2025-11-16 15:51:43 +0530

    func

--- Section 2/Cards/main.go
+++ Section 2/Cards/main.go
@@ -2,9 +2,17 @@ package main
 
 import "fmt"
 
+var card string = "Ace of Spades"
+
+// card := "Ace of Spades"
+
 func main() {
-	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	card = "Five of diimonds"
+	// card := "Ace of Spades"
+	// card = "Five of diimonds"
+	card = newCard()
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return " new Card"
 }
