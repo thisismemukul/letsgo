544d886d940352e75ecff2bd65c4f8810bc6540b
thisismemukul <thisismemukul@gmail.com>
2025-11-19 14:15:20 +0530

    recivers func

--- Section 2/Cards/slices.go
+++ Section 2/Cards/slices.go
@@ -1,12 +1,8 @@
 package main
 
-import "fmt"
-
 func slice() {
-	cards := []string{"1", newCard()}
+	cards := deck{"1", newCard()}
 	cards = append(cards, "2")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
